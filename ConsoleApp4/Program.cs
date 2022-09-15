2d981c9371ebad1e3ffc12b2f30f5ba93c4453d0
zaqlp <vavrazbynek.56@gmail.com>
2022-09-15 15:59:21 +0200

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string zadanecislo;
+long i;
+long start = 0;
+long pocet = 0;
+zadanecislo =Console.ReadLine();
+start= long.Parse(zadanecislo);
+
+    
+    i = start;
+    pocet = 0;
+    while (i > 1)
+    {
+
+        if (i % 2 == 0)
+        {
+
+            i = i / 2;
+
+            pocet++;
+        }
+        else
+        {
+
+            i = i * 3;
+            i++;
+
+            pocet++;
+        }
+    }
+
+
+
+Console.WriteLine(pocet);
+
